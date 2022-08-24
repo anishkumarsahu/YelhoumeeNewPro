2f1784bbb6c1b1f35f675d8abd0d888e4f8e51ff
anishkumarsahu <anishkumarsahu03@gmail.com>
2022-08-24 10:28:48 +0530

    added database settings for hosting

--- pro/settings.py
+++ pro/settings.py
@@ -74,32 +74,31 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pro.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'ecom',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#         'USER': 'root',
-#         'PASSWORD': 'pass',
-#         'OPTIONS': {
-#             "init_command": "SET foreign_key_checks = 0;",
-#             'charset': 'utf8mb4',
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yelhoumee',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'pass',
+        'OPTIONS': {
+            "init_command": "SET foreign_key_checks = 0;",
+            'charset': 'utf8mb4',
+        },
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
